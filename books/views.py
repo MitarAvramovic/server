627111e8543720c4f14c33430bd7e9e4ee5ee2d9
MitarAvramovic <mitar10.avramovic@gmail.com>
2026-04-28 23:47:01 +0200

    Add POST method

--- books/views.py
+++ books/views.py
@@ -1,12 +1,28 @@
 # books/views.py
 
+from django.views.decorators.csrf import csrf_exempt
+
+
 import json
 from django.http import JsonResponse
 from django.views import View
 from .models import Book
 
 
+@csrf_exempt
 def book_list(request):
-    books = Book.objects.all().values("id", "title", "author")
-    print(list(books))
-    return JsonResponse(list(books), safe=False)
+    if request.method == "GET":
+        books = Book.objects.all().values("id", "title", "author")
+        print(list(books))
+        return JsonResponse(list(books), safe=False)
+
+    elif request.method == "POST":
+        print("POST radi")
+        data = json.loads(request.body)
+
+        title = data.get("title")
+        author = data.get("author")
+
+        book = Book.objects.create(title=title, author=author)
+
+        return JsonResponse({"id": book.id, "title": book.title, "author": book.author})
